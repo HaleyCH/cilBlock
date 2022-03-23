31256852b8d1127fcc80cef0242c486f469a8be5
HaleyC <1617335718@qq.com>
2022-03-23 22:44:42 +0800

    record the last statue of GM

--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ int main() {
     GM.start();
     std::cout<<(GM.getState()==GAME_STATE::STOP?0:1)<< std::endl;
     GM.run();
-    GM.log();
+    GM.log(DEBUG_LEVEL::ERROR);
     fclose(stderr);
 }
