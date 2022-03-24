50e2c17b6054859cbc1ffa5562b162c680e06353
HaleyC <1617335718@qq.com>
2022-03-24 17:34:28 +0800

    Fix a priority error and make functions to be static.

--- Debug.h
+++ Debug.h
@@ -8,20 +8,20 @@
 #include "string"
 
 enum class DEBUG_LEVEL{
-    INFO=0,
+    CRITICAL=0,
+    PUBLISH,
+    INFO,
     LOG,
     MESSAGE,
     DEBUG,
     ERROR,
-    PUBLISH,
-    CRITICAL
 };
 
 class Debug{
 private:
     static FILE *_outputStream;
     static DEBUG_LEVEL debugLevel;
-    void _outputOrIgnore(const char *msg, DEBUG_LEVEL level);
+    static void _outputOrIgnore(const char *msg, DEBUG_LEVEL level);
     static void _runFunctionOrIgnore(bool (*f)(), DEBUG_LEVEL level);
     static void _format();
 public:
@@ -30,12 +30,12 @@ public:
     ~Debug()=default;
     static void init();
     static void init(const char *fp, const char *mode="w", DEBUG_LEVEL level=DEBUG_LEVEL::PUBLISH);
-    void info(const char *msg);
-    void log(const char *msg);
-    void msg(const char *msg);
-    void debug(const char *msg);
-    void error(const char *msg);
-    void critical(const char *msg);
+    static void info(const char *msg);
+    static void log(const char *msg);
+    static void msg(const char *msg);
+    static void debug(const char *msg);
+    static void error(const char *msg);
+    static void critical(const char *msg);
 
     static void info(bool (*f)());
     static void log(bool (*f)());
